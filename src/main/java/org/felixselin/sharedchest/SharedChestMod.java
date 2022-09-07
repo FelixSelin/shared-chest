6b6922b02e17e87ffc988e0fa9675bf157e80bbe
felix <tigroff.mike@gmail.com>
2022-09-08 00:14:02 +0300

    Project setup

---
+++ src/main/java/org/felixselin/sharedchest/SharedChestMod.java
@@ -0,0 +1,15 @@
+package org.felixselin.sharedchest;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SharedChestMod implements ModInitializer {
+	public static final String MOD_ID = "sharedchestmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/org/felixselin/sharedchest/SharedChestModClient.java
@@ -0,0 +1,10 @@
+package org.felixselin.sharedchest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SharedChestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
